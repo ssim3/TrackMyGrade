9c9ee8f7455ce46ce0841bb625b56677f943d466
Tristan Sim <93330584+ssim3@users.noreply.github.com>
2022-02-16 14:46:54 +0800

    Some shit

--- rater.py
+++ rater.py
@@ -1,6 +1,8 @@
+from turtle import title
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pprint
+from googleapiclient import discovery
 
 # Authorizing the API
 
@@ -9,9 +11,11 @@ file_name = r"C:\Users\Tristan Sim\Downloads\Kali\client_key.json"
 creds = ServiceAccountCredentials.from_json_keyfile_name(file_name,scope)
 client = gspread.authorize(creds)
 
-#Fetch the sheet
-sheet = client.open('LEAGUE CHAMP RATER').sheet1
-python_sheet = sheet.get_all_records(["A2:C154"])
-print(python_sheet)
+service = discovery.build("sheets", "v4", credentials=creds)
+
+spreadsheet = {"properties": {"title": title}}
+
+spreadsheet = service.spreadsheets().create(body = spreadsheet, fields = "Grade Tracker").execute()
+print(spreadsheet.get("Grade Tracker"))
 
     
